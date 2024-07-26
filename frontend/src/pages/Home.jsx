fee415318052ebf6b28b66e3b8237f698d469aca
saleemjavid360 <msaleem@kodecorp.com>
2024-07-26 11:19:07 +0530

    First Commits

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+import api from "../api";
+import "../styles/Home.css"
+import Note from "../components/Note";
+function Home() {
+  const [notes, setNotes] = useState([]);
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+  const getNotes = () => {
+    api
+      .get("/api/notes/")
+      .then((res) => res.data)
+      .then((data) => setNotes(data))
+      .catch((err) => alert(err));
+  };
+
+  const deleteNote = () => {
+    api
+      .delete(`/api/notes/delete/${id}`)
+      .then((res) => {
+        if (res.status === 204) alert("Note deleted");
+        else alert("Field to delete note.");
+        getNotes();
+      })
+      .catch((error) => alert(error));
+    
+  };
+
+  const createNote = (e) => {
+    e.preventDefault();
+    api
+      .post("/api/notes/", { content, title })
+      .then((res) => {
+        if (res.status === 201) alert("Note Created");
+        else alert("Failed to make note.");
+        getNotes();
+      })
+      .catch((err) => alert(err));
+    
+  };
+  return (
+    <div className="">
+      <div className="">
+        <h2>Notes</h2>
+        {notes.map((note)=><Note note={note} onDelete={deleteNote} key={note.id}/>)}
+      </div>
+      <h2>Create a Note</h2>
+      <form onSubmit={createNote}>
+        <label htmlFor="title">Title:</label>
+        <br />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <label htmlFor="content">Content:</label>
+        <br />
+        <textarea
+          name="content"
+          id="content"
+          required
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+export default Home;
---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function NotFound() {
+    return <div className="">
+        <h1>404 Not Found</h1>
+        <p>The Page Your're looking for doesn't exist!</p>
+    </div>
+}
+
+export default NotFound
